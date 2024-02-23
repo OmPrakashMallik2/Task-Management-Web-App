d29145927a587f236661fc913321a4f5844a093f
Om Prakash Mallik <opm202000@gmail.com>
2024-02-23 19:59:08 +0530

    building task management web app

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-blue-600 font-bold text-center text-5xl' >Task Management Web App💥</h1>
+      <h1 className='text-blue-600 font-bold text-center text-5xl' >Task Management Web App💥</h1>
+      <h1 className='text-blue-600 font-bold text-center text-5xl' >Task Management Web App💥</h1>
+      <h1 className='text-blue-600 font-bold text-center text-5xl' >Task Management Web App💥</h1>
+    </div>
+  );
+}
+
+export default App;
